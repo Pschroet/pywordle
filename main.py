6ae7bd8288ba602a005b319a93a2cac38c10197d
Pschroet <Pschroet@users.noreply.github.com>
2022-10-08 23:20:35 +0200

    refactored word getting module to be just functions and added the source of the words list as an argument to the main module, either a file or a URL

--- main.py
+++ main.py
@@ -14,19 +14,27 @@ if __name__ == '__main__':
     parser.add_argument("-e", "--excluded", action="store_true", default=False, help="show a list with the wrong letters after each guess")
     parser.add_argument("-s", "--show_hits", action="store_true", default=False, help="show the hits in an extra list after each guess")
     parser.add_argument("-l", "--loop", action="store_true", default=False, help="Run in a loop, quit with Ctrl+Q")
+    parser.add_argument("-f", "--file", action="store", help="A file with a word list (has higher priority if passed than URL)")
+    parser.add_argument("-u", "--url", action="store", help="A url to a file with a word list (has lower priority than file)", default="https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/28804271b5a226628d36ee831b0e36adef9cf449/wordle-answers-alphabetical.txt")
     args = parser.parse_args()
-    if hasattr(args, "loop") and args.loop:
-        print("Running in a loop. Exit with Ctrl+Q.")
-        do_run = True
-        while do_run:
-            try:
-                wordle_word = word.wordleWord().get_new_word()
-                input_handler = handler.inputHandler(word=wordle_word, show_exluded=args.excluded, show_hits=args.show_hits)
-                input_handler.start()
-            except KeyboardInterrupt:
-                do_run = False
-                print(os.linesep + "Exiting guessing... (Just fyi the word was " + str(wordle_word) + ")")
+    wordle_word = None
+    if hasattr(args, "file"):
+        wordle_word = word.get_new_word_from_file(args.file)
+    elif hasattr(args, "url"):
+        wordle_word = word.get_new_word_from_url(args.url)
+    if wordle_word is not None:
+        if hasattr(args, "loop") and args.loop:
+            print("Running in a loop. Exit with Ctrl+Q.")
+            do_run = True
+            while do_run:
+                try:
+                    input_handler = handler.inputHandler(word=wordle_word, show_exluded=args.excluded, show_hits=args.show_hits)
+                    input_handler.start()
+                except KeyboardInterrupt:
+                    do_run = False
+                    print(os.linesep + "Exiting guessing... (Just fyi the word was " + str(wordle_word) + ")")
+        else:
+            input_handler = handler.inputHandler(word=wordle_word, show_exluded=args.excluded, show_hits=args.show_hits)
+            input_handler.start()
     else:
-        wordle_word = word.wordleWord().get_new_word()
-        input_handler = handler.inputHandler(word=wordle_word, show_exluded=args.excluded, show_hits=args.show_hits)
-        input_handler.start()
+        print("Can't start game, because no word was returned")
--- word.py
+++ word.py
@@ -4,19 +4,25 @@ Created on 19.04.2022
 @author: philipp schroeter
 '''
 
+import os
 import random
 import requests
 
-class wordleWord():
-
-    def __init__(self):
-        self.wordle_list_eng = "https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/28804271b5a226628d36ee831b0e36adef9cf449/wordle-answers-alphabetical.txt"
-
-    def get_new_word(self):
-        wordle_words = requests.get(self.wordle_list_eng)
-        #print(wordle_words.content)
-        words_list = wordle_words.content.decode("utf-8").split("\n")
+def get_new_word_from_file(file=None, debug=False):
+    if file is not None and os.path.isfile(file):
+        wordle_words = open(file).read()
+        words_list = wordle_words.split(os.linesep)
         #print(words_list)
         searched_word = words_list[random.randint(0, len(words_list))]
-        #print("searched word is " + str(searched_word))
+        if debug: print("searched word is " + str(searched_word))
         return searched_word
+    return None
+
+def get_new_word_from_url(url=None, debug=False):
+    wordle_words = requests.get(url)
+    #print(wordle_words.content)
+    words_list = wordle_words.content.decode("utf-8").split("\n")
+    #print(words_list)
+    searched_word = words_list[random.randint(0, len(words_list))]
+    if debug: print("searched word is " + str(searched_word))
+    return searched_word
